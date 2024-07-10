f440862fd105350643c94d8a71890838f5c637c4
thearhansheth <arhan.sheth@gmail.com>
2024-07-10 12:33:59 +0530

    Issue with records in csv_list

--- url_main.py
+++ url_main.py
@@ -21,11 +21,15 @@ with open(filepath2, mode = 'r', encoding = 'utf-8') as file2:
     for i in range (4, len(list2)):
         url_list2.append(list2[i][3])
 
+#introducing final list to seperate unique urls
 final_list = []
+
+# processing unique urls from file 1
 for i in url_list1:
     if i not in final_list:
         final_list.append(i)
 
+# processing unique urls from file 2
 for j in url_list2:
     if j not in final_list:
         final_list.append(j)
@@ -40,6 +44,29 @@ with open(master_file, mode = 'r', encoding = 'Windows-1252') as file3:
     for i in range (1, len(list3)):
         url_list3.append(list3[i][24])
 
+# processing unique urls from master file
 for x in url_list3:
     if x not in final_list:
         final_list.append(x)
+
+# initialize csv_list as list of all unique records
+csv_list = []
+
+# import all unique records into csv_list from all files
+for i in range(4, len(list1)):
+    if list1[i][6] in final_list:
+        if list1[i] not in csv_list:
+            csv_list.append(list1[i])
+
+for j in range(4, len(list2)):
+    if list2[j][3] in final_list:
+        if list2[j] not in csv_list:
+            csv_list.append(list2[j])
+
+for x in range(1, len(list3)):
+    if list3[x][24] in final_list:
+        if list3[x] not in csv_list:
+            csv_list.append(list3[x])
+
+print(len(final_list))
+print(len(csv_list))
